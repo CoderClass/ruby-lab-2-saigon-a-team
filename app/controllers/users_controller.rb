65de1f4d63228429fba85e96246b0b919007c78c
Khoa Bui <buihdk@gmail.com>
2016-10-19 19:53:16 +0700

    worked on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "User was created successfully"
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      flash[:error] = "Error: #{@user.errors.full_messages.to_sentence}"
+      redirect_to users_path 
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :image_url)
   end
 
   def index
